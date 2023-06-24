34e620054706795dcdd03e919c1907fb3fcf59a4
Rutileno Gabriel <92885893+BigLeno@users.noreply.github.com>
2023-06-24 00:37:48 -0300

    Melhorando a documentação

--- lib/Arm/Arm.h
+++ lib/Arm/Arm.h
@@ -9,18 +9,18 @@ private:
 
 public:
   /**
-   * Construtor da classe Arm.
-   * Inicializa os motores do braço.
+   * @brief Construtor da classe Arm e inicializa os motores do braço.
+   * 
    */
   Arm();
 
   /**
-   * Leva os motores do braço à posição inicial.
+   * @brief Leva os motores do braço à posição inicial.
    */
   void home();
 
   /**
-   * Define a posição individualmente para um motor específico.
+   * @brief Define a posição individualmente para um motor específico.
    *
    * @param motorNum O número do motor (0 a 5).
    * @param position A posição desejada para o motor.
@@ -28,7 +28,7 @@ public:
   void setIndividualPosition(uint8_t motorNum, uint8_t position);
 
   /**
-   * Define a posição para cada motor do braço separadamente.
+   * @brief Define a posição para cada motor do braço separadamente.
    *
    * @param position0 A posição desejada para o Motor 0.
    * @param position1 A posição desejada para o Motor 1.
@@ -40,7 +40,7 @@ public:
   void setArmPosition(uint8_t position0, uint8_t position1, uint8_t position2, uint8_t position3, uint8_t position4, uint8_t position5);
 
   /**
-   * Define a posição para cada motor do braço usando um vetor de posições.
+   * @brief Define a posição para cada motor do braço usando um vetor de posições.
    *
    * @param positions Um vetor contendo as posições desejadas para cada motor do braço.
    *                  O vetor deve ter 6 elementos.
